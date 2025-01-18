b13ee7e70f86a26abe64119e5d245c7a630d932f
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-01-18 15:15:49 +0800

    add user admin

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'full_name', 'phone_number', 'location', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'location')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number')
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'location', 'pfp')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number'),
+        }),
+    )
