08e54a4a1a1ab20e6573940431f1672c144a57d4
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-01-18 15:10:39 +0800

    fix errors

--- src/haircat/settings.py
+++ src/haircat/settings.py
@@ -63,6 +63,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "haircat.urls"
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = "haircat.wsgi.application"
 
 
@@ -131,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom user model
-AUTH_USER_MODEL = "accounts.CustomUser"
+AUTH_USER_MODEL = "account.CustomUser"
 
 # Rest framework settings
 REST_FRAMEWORK = {
