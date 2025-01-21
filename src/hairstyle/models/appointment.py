c536cd25359c80a25b341ce5c788bed854431684
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-01-21 09:59:02 +0800

    format project

--- src/hairstyle/models/appointment.py
+++ src/hairstyle/models/appointment.py
@@ -8,6 +8,7 @@ from hairstyle.models.service import Service
 
 User = get_user_model()
 
+
 class Appointment(models.Model):
     PENDING = 0
     CONFIRMED = 1
@@ -21,8 +22,12 @@ class Appointment(models.Model):
         (CANCELLED, "Cancelled"),
     ]
 
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="Appointments")
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name="Appointments")
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="Appointments"
+    )
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="Appointments"
+    )
     schedule = models.DateTimeField()
     status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
     notes = models.TextField(max_length=255, null=True, blank=True)
@@ -33,7 +38,7 @@ class Appointment(models.Model):
     @property
     def specialist(self):
         return self.service.specialist
-    
+
     @property
     def status_display(self):
         return dict(self.STATUS_CHOICES)[self.status]
@@ -44,8 +49,12 @@ class Appointment(models.Model):
 
 class Review(models.Model):
 
-    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, related_name="Reviews")
-    rating = models.IntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    appointment = models.ForeignKey(
+        Appointment, on_delete=models.CASCADE, related_name="Reviews"
+    )
+    rating = models.IntegerField(
+        default=5, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     comment = models.TextField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -55,7 +64,9 @@ class Review(models.Model):
 
 
 class Message(models.Model):
-    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, related_name="Messages")
+    appointment = models.ForeignKey(
+        Appointment, on_delete=models.CASCADE, related_name="Messages"
+    )
     message = models.TextField(max_length=255)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Messages")
     schedule_change_req = models.DateTimeField(null=True, blank=True)
