9bde884c8b117c024e5a76129a52d84bd08e23e6
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-01-21 09:58:19 +0800

    modify settings

--- src/haircat/settings.py
+++ src/haircat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7zvz5w98n+6$zm@%0zgy1mpi3l4@#4ymbti0hh0ceew$z+8@6l"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
