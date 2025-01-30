5353cc47a096bb0a5c8f222b5feccf64c27b6007
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-01-30 22:11:26 +0800

    create review view

--- src/hairstyle/views/appointment.py
+++ src/hairstyle/views/appointment.py
@@ -1,8 +1,18 @@
-from hairstyle.serializers.appointment import AppointmentSerializer
-from hairstyle.models.appointment import Appointment
+from hairstyle.serializers.appointment import AppointmentSerializer, ReviewImageSerializer, ReviewSerializer
+from hairstyle.models.appointment import Appointment, Review, ReviewImage, Message
 from haircat.utils import GenericView
 
 
 class AppointmentView(GenericView):
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
+
+
+class ReviewView(GenericView):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+
+class ReviewImageView(GenericView):
+    serializer_class = ReviewImageSerializer
+    queryset = ReviewImage.objects.all()
+    allowed_methods = ["list", "create", "delete"]
