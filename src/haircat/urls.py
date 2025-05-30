0a8881271651e002f85f081aa1df548220bdf9c4
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-05-31 07:22:39 +0800

    Add health check endpoint to URLs for service monitoring

--- src/haircat/urls.py
+++ src/haircat/urls.py
@@ -19,12 +19,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("account.urls")),
     path("api/", include("hairstyle.urls")),
     path("api/", include("general.urls")),
+    path("health/", health_check, name="health-check"),
 ]
 
 # Serve static files in development
